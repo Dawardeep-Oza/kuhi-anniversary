b4e62d28f60aa9355bcdbb6aefc7726a2ab9ec22
Dawardeep-Oza <123dawardeepoza@gmail.com>
2026-09-04 12:47:06 +0530

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/kuhi-anniversary/',
   plugins: [react()],
   resolve: {
     alias: {
